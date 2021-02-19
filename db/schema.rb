75e088d0a40b9cd91d334962a0196fa110958e00
Max Beneke <max.beneke@gmail.com>
2021-02-19 09:12:20 -0500

    More Css and Pictures

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_17_154946) do
+ActiveRecord::Schema.define(version: 2021_02_19_134443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_02_17_154946) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "abbr"
+    t.string "picture"
   end
 
   create_table "flights", force: :cascade do |t|
