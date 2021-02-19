f08824ca6ec0bb56dd89d0ef9965ba1401847802
Max Beneke <max.beneke@gmail.com>
2021-02-19 10:38:03 -0500

    Final Touches

--- app/models/vacation.rb
+++ app/models/vacation.rb
@@ -5,7 +5,7 @@ class Vacation < ApplicationRecord
   has_one :departure_flight, through: :departure, source: :departure_flight
   has_one :arrival, dependent: :destroy
   has_one :arrival_flight, through: :arrival, source: :arrival_flight 
-  has_many :vacation_activities
+  has_many :vacation_activities, dependent: :destroy
   has_many :activities, through: :vacation_activities
 
   validates :user_id, presence: true  
